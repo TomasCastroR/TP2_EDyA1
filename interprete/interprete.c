bb36bc15387cd7f51fed3d10177c3da51fb95aad
FranKKastor <FranKKastor@users.noreply.github.com>
2020-05-31 17:58:22 -0300

    balancear un arbol no es trivial, leer comandos menos

---
+++ interprete/interprete.c
@@ -0,0 +1,14 @@
+#include "../itree/itree.h"
+#include <stdio.h>
+
+
+int main () {
+  ITree arbol= itree_crear();
+  char resto[10];
+  int comando;
+  double intervaloIzq, intervaloDer;
+  int CONDICION = 1;
+  printf ("ingrese un comando: ");
+  scanf ("%[^]", resto);
+  printf ("%s\n", resto);
+}
